47549295985cb959e0e70fff853e86cb7f38c745
biancaligmon601 <biancaligmon93@gmail.com>
2025-02-13 18:13:52 +0800

    added java application

---
+++ Ligmon_IT13A_activities/src/main/java/com/mycompany/ligmon_it13a_activities/Ligmon_IT13A_activities.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ligmon_it13a_activities;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Ligmon_IT13A_activities {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

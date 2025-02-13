b65e5bbdf03be037bfe2534d3f8d8e83c1d69f63
biancaligmon601 <biancaligmon93@gmail.com>
2025-02-13 18:40:11 +0800

    added hello world class

---
+++ Ligmon_IT13A_activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Lab_act1_Arithmetics {
+     public static void main(String []args){
+     int firstnumber = 10;
+     int secondnumber = 4;
+     int thirdnumber = 6;
+        
+     int addition = firstnumber + secondnumber + thirdnumber;
+     int subtraction = firstnumber - secondnumber - thirdnumber;
+     int multiply = firstnumber * secondnumber * thirdnumber;
+     int divide = firstnumber / secondnumber / thirdnumber;
+     int module = firstnumber & secondnumber & thirdnumber;
+     
+     System.out.println("10 + 4 + 6 = " + addition);
+     System.out.println("10 - 4 - 6 = " + subtraction);
+     System.out.println("10 * 4 * 6 = " + multiply);
+     System.out.println("10 / 4 / 6 = " + divide);
+     System.out.println("10 & 4 & 6 = " + module);
+     
+        
+    }
+}
